403a4872ef190a842ea34d1188192c0fccd18cd0
Bharath18sv <svbharath2005@gmail.com>
2025-11-20 20:25:30 +0530

    Add starter code for unimplemented pages

--- frontend/src/app/doctor/summary/[id]/page.jsx
+++ frontend/src/app/doctor/summary/[id]/page.jsx
@@ -1,3 +1,11 @@
 //view a summary of a patient
 
 // display summary details, patient details, doctor details, and option to download/print the summary
+
+import React from "react";
+
+function page() {
+  return <div>Under development...</div>;
+}
+
+export default page;
