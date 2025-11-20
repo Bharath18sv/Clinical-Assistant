ddc83e172bcdd7c6d093104355b1c42ce17a4439
Bharath18sv <svbharath2005@gmail.com>
2025-11-20 20:27:02 +0530

    Minor fix

--- frontend/src/app/doctor/summary/page.jsx
+++ frontend/src/app/doctor/summary/page.jsx
@@ -1,1 +1,13 @@
 //view all list of summaries of a doctor
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1>All Summaries</h1>
+      <p>Under development...</p>
+    </div>
+  )
+}
+
+export default page
