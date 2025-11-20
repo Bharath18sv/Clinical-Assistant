da8a86bb39c53ce3d82d39eb08f559f22ea0613c
Bharath18sv <svbharath2005@gmail.com>
2025-11-20 19:43:28 +0530

    Fix page not found errors

--- frontend/src/app/admin/appointments/page.jsx
+++ frontend/src/app/admin/appointments/page.jsx
@@ -1,1 +1,8 @@
 //view all appointments, total appointments, appointments by status
+import React from "react";
+
+function page() {
+  return <div>Under development...</div>;
+}
+
+export default page;
